9b172caff6455dc057af8fce12b7c8b9dd5517cf
saichaitanya518 <saichaitanya518@gmail.com>
2024-04-21 20:52:28 +0530

    initailly started

---
+++ src/test/java/com/chaitu/blogappspringboot/BlogappSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaitu.blogappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogappSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
